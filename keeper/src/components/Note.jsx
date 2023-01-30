6a60f95b383495956f2239a153b000db8d20cc35
Nuwandz <Nuwandz96@gmail.com>
2023-01-30 16:35:40 +0530

    Created note.jsx file

---
+++ keeper/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default  function Note () {
+
+    return <div>
+ 
+      <h1>This is the titile</h1>        
+       <p>Thi sis the content</p>
+    </div>
+}
+
+
