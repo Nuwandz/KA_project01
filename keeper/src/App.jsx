a9f25d4197b0742ab581c6f1f75aeeeb8443da36
Nuwandz <Nuwandz96@gmail.com>
2023-01-30 15:12:58 +0530

    edited app.jsx

--- keeper/src/App.jsx
+++ keeper/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import Footer  from "./components/App";
+import Footer  from "./components/Footer";
 
 export default function App () {
     return (<div>
--- keeper/src/components/Footer.jsx
+++ keeper/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export default function footer () {
 
     const currentYear = new Date().getFullYear();
