ec5f41b3fee0e29a361f430697aac5902d6fbe3a
Nuwandz <Nuwandz96@gmail.com>
2023-01-30 15:35:59 +0530

    error fixed in app.jsx

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import Footer  from "./components/Footer";
+import Footer  from "./Footer";
 
 export default function App () {
     return (<div>
--- keeper/src/components/Footer.jsx
+++ keeper/src/components/Footer.jsx
@@ -4,5 +4,6 @@ import React from "react";
 export default function footer () {
 
     const currentYear = new Date().getFullYear();
+    
     return <p> Copyright © 2019 {currentYear}  </p>
 }
