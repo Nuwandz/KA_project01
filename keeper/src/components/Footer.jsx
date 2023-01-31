5f910d2f8b81e3ddaeb16a9dc88ec9971ae32f49
Nuwandz <Nuwandz96@gmail.com>
2023-01-31 08:58:25 +0530

    athugawa

--- keeper/src/components/Footer.jsx
+++ keeper/src/components/Footer.jsx
@@ -4,6 +4,8 @@ import React from "react";
 export default function footer () {
 
     const currentYear = new Date().getFullYear();
-    
-    return <p> Copyright © 2019 {currentYear}  </p>
+
+    return  (
+    <footer><p> Copyright ©  {currentYear}  </p></footer>
+    );
 }
